341b49022ea6980ee9a9e736240ac6d6974ae3e3
burbruee <burbruee@gmail.com>
2024-05-27 21:01:28 +0200

    Update fast-lakes.js
    Restructured code in order to reuse the patch bytes.

--- patch/optional/fast-lakes.js
+++ patch/optional/fast-lakes.js
@@ -4,57 +4,35 @@ module.exports = {
 	description: 'Open lakes right away if you have Blue Crystal+, you don\'t even need to equip it!',
 	//qol, random, difficulty, misc
 	type: 'qol',
-    character: '1',
-	patch: [
-		//Yuba Lake
-		{ offset: 0x679D, bytes: [
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xA5, 0x91,		//lda $91 (InventoryBodyParts1)
-			0x29, 0x60,		//and #$60
-			0xC9, 0x40,		//cmp #$40
-			0x90, 0x1C,		//bcc ($A7B7)
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA,			//nop
-			0xEA			//nop
-		],
-		//Uta Lake
-		offset: 0x6DCC, bytes: [
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xA5, 0x91,
-			0x29, 0x60,
-			0xC9, 0x40,
-			0x90, 0x1C,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA,
-			0xEA
-		  ]}
-	]
+	character: '1',
+	patch: function (pm) {
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// LDA $91 ; (InventoryBodyParts1)
+		// AND #$60
+		// CMP $#40
+		// BCC 			; $A7B7
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		// NOP
+		var patch = [0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xA5, 0x91, 0x29, 0x60, 0xC9, 0x40, 0x90, 0x1C, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA, 0xEA];
+
+		//Patch Yuba lake (near Rover)
+		pm.add(patch, 0x679D);
+		//Patch Uta lake (near Bodley)
+		pm.add(patch, 0x6DCC);
+	}
 };
